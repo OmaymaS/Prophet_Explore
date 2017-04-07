3d4e00d10035ad15e731607166f7c0dc83b45ecc
Omayma Said <OmaymaS@users.noreply.github.com>
2017-04-07 18:18:53 +0200

    reorder code and edit ch_points_param

--- prophet_explore/app.R
+++ prophet_explore/app.R
@@ -2,115 +2,123 @@
 library(shiny)
 library(dplyr)
 library(prophet)
+library(DT)
 library(shinythemes)
 
 
 # UI ------------------------------
 ui <- fluidPage(
-
+        
         # Application title
         titlePanel("Prophet Explore"),
         
         # Sidebar -------------------------------------
         fluidPage(theme = shinytheme("flatly"),
-                sidebarPanel(width=3,
-                             
-                             tabsetPanel(tabPanel(HTML("prophet <br> Parameters"),
-                                                  
-                                                  # paramter: growth
-                                                  radioButtons("growth","growth",
-                                                               c('linear','logistic'), inline = TRUE),
-                                                  
-                                                  # date input
-                                                  dateInput("ch_date", "Add changepoints",value=NULL),
-                                                  
-                                                  # paramter: changepoints
-                                                  textInput("changepoints","changepoints",NULL),
-                                                  
-                                                  uiOutput("ch_points"),
-                                                  
-                                                  # parameter: n.changepoints
-                                                  numericInput("n.changepoints","n.changepoints", value = 25),
-                                                  
-                                                  # parameter: yearly.seasonality
-                                                  checkboxInput("yearly","yearly.seasonality", value = TRUE),
-                                                  
-                                                  # parameter: weekly.seasonality 
-                                                  checkboxInput("monthly","weekly.seasonality", value = TRUE),
-                                                  
-                                                  # parameter: holidays
-                                                  
-                                                  # parameter: seasonality.prior.scale
-                                                  numericInput("seasonality_scale","seasonality.prior.scale", value = 10),
-                                                  
-                                                  # parameter: changepoint.prior.scale
-                                                  numericInput("changepoint_scale","changepoint.prior.scale", value = 0.05, step = 0.01),
-                                                  
-                                                  # parameter: holidays.prior.scale
-                                                  numericInput("holidays_scale","holidays.prior.scale", value = 10),
-                                                  
-                                                  # parameter: mcmc.samples
-                                                  numericInput("mcmc.samples", "mcmc.samples", value = 0),
-                                                  
-                                                  # parameter: interval.width
-                                                  numericInput("interval.width", "interval.width", value= 0.8, step = 0.1),
-                                                  
-                                                  # parameter: uncertainty.samples
-                                                  numericInput("uncertainty.samples","uncertainty.samples", value = 1000),
-                                                  
-                                                  # parameter: fit
-                                                  checkboxInput("fit", "fit", value = TRUE)),
-                                         
-                                         tabPanel(HTML("predict <br> Parameters"),
-                                                  
-                                                  # paramater: periods
-                                                  numericInput("periods","periods",value=365),
-                                                  
-                                                  # parameter: freq
-                                                  selectInput("freq","freq",
-                                                              choices = c('day', 'week', 'month', 'quarter','year')),
-                                                  # parameter: include_history
-                                                  checkboxInput("include_history","include_history", value = TRUE)
-                                                  ))
-                             
-                             
-                             # h3("prophet Parameters"),
-                             
-                             
-                             
-                             
-                             
-                ),
-                
-                # Main panel -------------------------
-                mainPanel(
-                        
-                        fluidRow(
-                                ## upload file -----------------
-                                column(width = 6,
-                                        fileInput("ts_file","Choose CSV File",
-                                                  accept = c(
-                                                          "text/csv",
-                                                          "text/comma-separated-values,text/plain",
-                                                          ".csv"))),
-                                ## plot buttion -----------------
-                                 column(width = 3,
-                                        actionButton("plot_btn2", "Plot",
-                                                     style = "width:100%; margin-top: 25px;"))),
-                        
-                        ## plot tabs --------------------------------
-                        fluidRow(column(width=12,
-                                        tabsetPanel(tabPanel("Forecast Plot",
-                                                             plotOutput("ts_plot")),
-                                                    tabPanel("Prophet Plot Components",
-                                                             plotOutput("prophet_comp_plot"))))
-                                 ),
-                        
-                        ## test output --------
-                        verbatimTextOutput("test")
-                        
-                        
-                )
+                  sidebarPanel(width=3,
+                               
+                               tabsetPanel(tabPanel(HTML("prophet <br> Parameters"),
+                                                    
+                                                    ## prophet() parameters ------------------------------
+                                                    ### paramter: growth
+                                                    radioButtons("growth","growth",
+                                                                 c('linear','logistic'), inline = TRUE),
+                                                    
+                                                    # ### date input
+                                                    # dateInput("ch_date", "Add changepoints",value=NULL),
+                                                    # 
+                                                    # ### paramter: changepoints
+                                                    # # textInput("changepoints","changepoints",NULL),
+                                                    # 
+                                                    # uiOutput("ch_points"),
+                                                    
+                                                    ### parameter: n.changepoints
+                                                    numericInput("n.changepoints","n.changepoints", value = 25),
+                                                    
+                                                    ### parameter: yearly.seasonality
+                                                    checkboxInput("yearly","yearly.seasonality", value = TRUE),
+                                                    
+                                                    ### parameter: weekly.seasonality 
+                                                    checkboxInput("monthly","weekly.seasonality", value = TRUE),
+                                                    
+                                                    ### parameter: holidays
+                                                    
+                                                    ### parameter: seasonality.prior.scale
+                                                    numericInput("seasonality_scale","seasonality.prior.scale", value = 10),
+                                                    
+                                                    ### parameter: changepoint.prior.scale
+                                                    numericInput("changepoint_scale","changepoint.prior.scale", value = 0.05, step = 0.01),
+                                                    
+                                                    ### parameter: holidays.prior.scale
+                                                    numericInput("holidays_scale","holidays.prior.scale", value = 10),
+                                                    
+                                                    ### parameter: mcmc.samples
+                                                    numericInput("mcmc.samples", "mcmc.samples", value = 0),
+                                                    
+                                                    ### parameter: interval.width
+                                                    numericInput("interval.width", "interval.width", value= 0.8, step = 0.1),
+                                                    
+                                                    ### parameter: uncertainty.samples
+                                                    numericInput("uncertainty.samples","uncertainty.samples", value = 1000),
+                                                    
+                                                    ### parameter: fit
+                                                    checkboxInput("fit", "fit", value = TRUE)),
+                                           
+                                           tabPanel(HTML("predict <br> Parameters"),
+                                                    
+                                                    ## make_future_dataframe() parameters ------------------
+                                                    ### paramater: periods
+                                                    numericInput("periods","periods",value=365),
+                                                    
+                                                    ### parameter: freq
+                                                    selectInput("freq","freq",
+                                                                choices = c('day', 'week', 'month', 'quarter','year')),
+                                                    ### parameter: include_history
+                                                    checkboxInput("include_history","include_history", value = TRUE)
+                                           ))
+                               
+                  ),
+                  
+                  # Main panel -------------------------
+                  mainPanel(
+                          
+                          fluidRow(
+                                  ## upload file -----------------
+                                  column(width = 6,
+                                         fileInput("ts_file","Choose CSV File",
+                                                   accept = c(
+                                                           "text/csv",
+                                                           "text/comma-separated-values,text/plain",
+                                                           ".csv"))),
+                                  ## plot button -----------------
+                                  column(width = 3,
+                                         actionButton("plot_btn2", "Plot",
+                                                      style = "width:100%; margin-top: 25px;"))),
+                          
+                          fluidRow(column(width = 6,
+                                          conditionalPanel("input.ch_points_param",
+                                                           dateInput("ch_date", "Add changepoints", value = NULL))
+                                          # uiOutput("date_in")
+                                          ),
+                                   
+                                   column(width = 6,uiOutput("ch_points"))
+                                   ),
+                          
+                          ## plot/results tabs --------------------------------
+                          fluidRow(column(width=12,
+                                          tabsetPanel(tabPanel("Forecast Plot",
+                                                               plotOutput("ts_plot")),
+                                                      tabPanel("Prophet Plot Components",
+                                                               plotOutput("prophet_comp_plot")),
+                                                      tabPanel("Forecase Table",
+                                                               dataTableOutput("data")))
+                          )
+                          ),
+                          
+                          ## test output --------
+                          verbatimTextOutput("test")
+                          
+                          
+                  )
         )
 )
 
@@ -123,10 +131,8 @@ server <- function(input, output, session) {
                 
                 file_in <- input$ts_file
                 
-                df <- read.csv(file_in$datapath, header = T) %>% 
+                read.csv(file_in$datapath, header = T) %>% 
                         mutate(y = log(y))
-                
-                return(df)
         })
         
         changepoints_vector <- reactive({
@@ -139,8 +145,8 @@ server <- function(input, output, session) {
                 else ch
         })
         
-        ## propher forecast -----------
-        prophet_model <- reactive({
+        ## create prophet model -----------
+        prophet_model <- eventReactive(input$plot_btn2,{
                 req(dat(),
                     input$n.changepoints,
                     input$seasonality_scale, input$changepoint_scale,
@@ -165,7 +171,7 @@ server <- function(input, output, session) {
         })
         
         ## Make dataframe with future dates for forecasting -------------
-        future <- reactive({
+        future <- eventReactive(input$plot_btn2,{
                 req(prophet_model(),input$periods, input$freq)
                 make_future_dataframe(prophet_model(),
                                       periods = input$periods,
@@ -189,29 +195,44 @@ server <- function(input, output, session) {
         ## plot prophet components --------------
         output$prophet_comp_plot <- renderPlot({
                 req(prophet_model(), forecast())
+                
                 prophet_plot_components(prophet_model(), forecast())
         })
         
-        
-        ## test op -------------------
-        output$test <- renderPrint(input$yearly
-                # changepoints_vector()
-                                   # prophet_model()[["changepoints"]]
-        )
-        
-        ## parameters ----------------
-        output$ch_points <- renderUI({
-                textAreaInput("ch_points_param","Selected Changepoints",value=NULL)
+        ## create datatabke from forecast dataframe --------------------
+        output$data <- renderDataTable({
+                datatable(forecast()) %>% 
+                        formatRound(columns=2:17,digits=4)
         })
         
-        ## update textArea
+        ## test op -------------------
+        output$test <- renderPrint(changepoints_vector())
+                                   # changepoints_vector()
+                                   # prophet_model()[["changepoints"]]
+        
+        
+        ## selected Changepoints ----------------
+        output$ch_points <- renderUI({
+                req(prophet_model())
+                textAreaInput("ch_points_param","Selected Changepoints",
+                              value=paste(prophet_model()$changepoints,collapse=", "))
+        })
+        
+        ## dateinput ---------------------
+        # output$date_in <- renderUI({
+        #         req(input$ch_points_param)
+        #         dateInput("ch_date", "Add changepoints", value = NULL)
+        # })
+        
+        
+        # update textArea
         observeEvent(input$ch_date,{
                 updateTextAreaInput(session,"ch_points_param",
                                     value=c(input$ch_points_param,
                                             as.character(as.Date(as.numeric(input$ch_date),
                                                                  origin="1970-01-01"))))
         })
-        
+
         
 }
 
