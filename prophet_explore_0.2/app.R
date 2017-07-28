d9b9dc2eaa8dbbc9d29c4cbe3f400e261c6e82d0
Omayma Said <OmaymaS@users.noreply.github.com>
2017-07-28 19:36:42 +0200

    add two tabs

--- prophet_explore_0.2/app.R
+++ prophet_explore_0.2/app.R
@@ -4,10 +4,13 @@ library(shinydashboard)
 library(shinyjs)
 library(dplyr)
 library(prophet)
+library(ggplot2)
 
 
 ui <- dashboardPage(
   dashboardHeader(title = "Prophet Explorer"),
+  
+  
   dashboardSidebar(
     # sidebarMenuOutput("menu")
     
@@ -41,7 +44,11 @@ ui <- dashboardPage(
   ),
   
   dashboardBody(
+    ## include css file --------------------
+    tags$head(tags$style(includeCSS("./www/mycss.css"))),
+    ## use shinyjs -----------------------
     useShinyjs(),
+
     fluidRow(
       column(width = 12,
              tabsetPanel(id = "inTabset",
@@ -80,86 +87,90 @@ ui <- dashboardPage(
                                                shinyjs::disabled(actionButton("next1", "Next",
                                                                               style = "width:100%; margin-top: 25px; font-size:200%"))))
                                   )
-                                  ),
+                         ),
                          ## TAB 2 -------------
                          tabPanel(title = "Set Parameters", value = "panel2",
                                   fluidRow(
                                     box(width = 12,
-                                    ## prophet parameters --------------------
-                                    box(width = 8,
-                                        column(width = 8, offset = 2,
-                                               tags$h3("Prophet Parameters")),
-                                        column(width = 6,
-                                               
-                                               radioButtons("growth","growth",
-                                                            c('linear','logistic'), inline = TRUE),
-                                               
-                                               ### parameter: yearly.seasonality
-                                               checkboxInput("yearly","yearly.seasonality", value = TRUE),
-                                               
-                                               ### parameter: weekly.seasonality 
-                                               checkboxInput("monthly","weekly.seasonality", value = TRUE),
-                                               ### parameter: n.changepoints
-                                               numericInput("n.changepoints","n.changepoints", value = 25),
-                                               
-                                               ### parameter: seasonality.prior.scale
-                                               numericInput("seasonality_scale","seasonality.prior.scale", value = 10),
-                                               
-                                               ### parameter: changepoint.prior.scale
-                                               numericInput("changepoint_scale","changepoint.prior.scale", value = 0.05, step = 0.01)),
-                                        column(width = 6,
-                                              
-                                               ### parameter: holidays.prior.scale
-                                               numericInput("holidays_scale","holidays.prior.scale", value = 10),
-                                               
-                                               ### parameter: mcmc.samples
-                                               numericInput("mcmc.samples", "mcmc.samples", value = 0),
-                                               
-                                               ### parameter: interval.width
-                                               numericInput("interval.width", "interval.width", value= 0.8, step = 0.1),
-                                               ### parameter: uncertainty.samples
-                                               numericInput("uncertainty.samples","uncertainty.samples", value = 1000))
-                                       
+                                        ## prophet parameters --------------------
+                                        box(width = 8,
+                                            column(width = 8, offset = 2,
+                                                   tags$h3("Prophet Parameters")),
+                                            column(width = 6,
+                                                   
+                                                   radioButtons("growth","growth",
+                                                                c('linear','logistic'), inline = TRUE),
+                                                   
+                                                   ### parameter: yearly.seasonality
+                                                   checkboxInput("yearly","yearly.seasonality", value = TRUE),
+                                                   
+                                                   ### parameter: weekly.seasonality 
+                                                   checkboxInput("monthly","weekly.seasonality", value = TRUE),
+                                                   ### parameter: n.changepoints
+                                                   numericInput("n.changepoints","n.changepoints", value = 25),
+                                                   
+                                                   ### parameter: seasonality.prior.scale
+                                                   numericInput("seasonality_scale","seasonality.prior.scale", value = 10),
+                                                   
+                                                   ### parameter: changepoint.prior.scale
+                                                   numericInput("changepoint_scale","changepoint.prior.scale", value = 0.05, step = 0.01)),
+                                            column(width = 6,
+                                                   
+                                                   ### parameter: holidays.prior.scale
+                                                   numericInput("holidays_scale","holidays.prior.scale", value = 10),
+                                                   
+                                                   ### parameter: mcmc.samples
+                                                   numericInput("mcmc.samples", "mcmc.samples", value = 0),
+                                                   
+                                                   ### parameter: interval.width
+                                                   numericInput("interval.width", "interval.width", value= 0.8, step = 0.1),
+                                                   ### parameter: uncertainty.samples
+                                                   numericInput("uncertainty.samples","uncertainty.samples", value = 1000))
+                                            
                                         ),
-                                    ## predict parameters --------------------
-                                    box(width = 4,
-                                        column(width = 12,
-                                               tags$h3("Predict Parameters")),
-                                        column(width = 12,
-                                               ### paramater: periods
-                                               numericInput("periods","periods",value=365),
-                                               
-                                               ### parameter: freq
-                                               selectInput("freq","freq",
-                                                           choices = c('day', 'week', 'month', 'quarter','year')),
-                                               
-                                               ### parameter: include_history
-                                               checkboxInput("include_history","include_history", value = TRUE))),
-                                    
-                                    ## Back/Next 2 --------------------------
-                                    box(width = 12,
-                                        column(width = 2, offset = 4,
-                                               actionButton("back2", "Back",
-                                                                              style = "width:100%; margin-top: 25px; font-size:200%")),
-                                        column(width = 2,
-                                               actionButton("next2", "Next",
-                                                                              style = "width:100%; margin-top: 25px; font-size:200%")))
-                                    
-                                 
-                                  ))),
+                                        ## predict parameters --------------------
+                                        box(width = 4,
+                                            column(width = 12,
+                                                   tags$h3("Predict Parameters")),
+                                            column(width = 12,
+                                                   ### paramater: periods
+                                                   numericInput("periods","periods",value=365),
+                                                   
+                                                   ### parameter: freq
+                                                   selectInput("freq","freq",
+                                                               choices = c('day', 'week', 'month', 'quarter','year')),
+                                                   
+                                                   ### parameter: include_history
+                                                   checkboxInput("include_history","include_history", value = TRUE))),
+                                        
+                                        ## Back/Next 2 --------------------------
+                                        box(width = 12,
+                                            column(width = 2, offset = 4,
+                                                   actionButton("back2", "Back",
+                                                                style = "width:100%; margin-top: 25px; font-size:200%")),
+                                            column(width = 2,
+                                                   actionButton("next2", "Next",
+                                                                style = "width:100%; margin-top: 25px; font-size:200%")))
+                                        
+                                        
+                                    ))),
                          ## TAB 3 -------------
                          tabPanel(title = "Fit Model", value = "panel3", 
                                   fluidRow(
                                     box(width = 12,
                                         column(width = 12,
-                                              shinyjs::disabled(actionButton("plot_btn2", "Fit Prophet Model",
-                                                                             style = "width:20%; margin-top: 25px;")
-                                                                )
+                                               shinyjs::disabled(actionButton("plot_btn2", "Fit Prophet Model",
+                                                                              style = "width:20%; margin-top: 25px;")
+                                               )
                                         ),
                                         column(width = 12, HTML("<br><br>")),
                                         column(width = 12,
-                                               DT::dataTableOutput("data"))
-                                        ),
+                                               conditionalPanel("input.plot_btn2",
+                                                                div(id = "output-container3",
+                                                                    tags$img(src = "spinner.gif",
+                                                                             id = "loading-spinner"),
+                                                                    DT::dataTableOutput("data"))))
+                                    ),
                                     
                                     box(width = 12,
                                         column(width = 2, offset = 4,
@@ -170,7 +181,47 @@ ui <- dashboardPage(
                                                             style = "width:100%; margin-top: 25px; font-size:200%")))
                                   )
                                   
-                                  )
+                         ),
+                         tabPanel(title = "Plot Results", value = "panel4",
+                                  fluidRow(
+                                    box(width =12, 
+                                        
+                                        column(width = 12,
+                                               shinyjs::disabled(actionButton("plot_btn3", "Plot Results",
+                                                                              style = "width:20%; margin-top: 25px;")
+                                               )
+                                        ),
+                                        column(width = 12, HTML("<br><br>")),
+                                        column(width=12,
+                                               tabsetPanel(
+                                                 tabPanel("Forecast Plot",
+                                                          conditionalPanel("input.plot_btn3",
+                                                                           div(id = "output-container",
+                                                                               tags$img(src = "spinner.gif",
+                                                                                        id = "loading-spinner")
+                                                                           ),
+                                                                           div(plotOutput("ts_plot"))
+                                                          )
+                                                 ),
+                                                 tabPanel("Prophet Plot Components",
+                                                          # output.logistic_check=='no_error'
+                                                          conditionalPanel("input.plot_btn3",
+                                                                           div(id = "output-container2",
+                                                                               tags$img(src = "spinner.gif",
+                                                                                        id = "loading-spinner"),
+                                                                               plotOutput("prophet_comp_plot"))
+                                                          )
+                                                 )
+                                               )
+                                        )
+                                    ),
+                                    
+                                    box(width = 12,
+                                        column(width = 2, offset = 4,
+                                               actionButton("back4", "Back",
+                                                            style = "width:100%; margin-top: 25px; font-size:200%"))
+                                    )
+                                  ))
              )
       )
       
@@ -198,6 +249,11 @@ server <- function(input, output, session) {
   observeEvent(input$back3, {
     updateTabsetPanel(session, "inTabset",
                       selected = "panel2")
+  })
+  
+  observeEvent(input$back4, {
+    updateTabsetPanel(session, "inTabset",
+                      selected = "panel3")
   })
   
   ## function: duplicatedRecative values -----------------------------
@@ -246,6 +302,8 @@ server <- function(input, output, session) {
       shinyjs::enable("plot_btn2")
   })
   
+  
+  
   ## logistic_check -------------------
   logistic_check <- eventReactive(input$plot_btn2, {
     # req(dat())
@@ -259,7 +317,7 @@ server <- function(input, output, session) {
   
   ## create prophet model -----------
   prophet_model <- eventReactive(input$plot_btn2,{
-
+    
     req(dat(), 
         # ("ds" %in% dat()), "y" %in% names(dat()),
         input$n.changepoints,
@@ -333,6 +391,28 @@ server <- function(input, output, session) {
     DT::datatable(forecast(), 
                   options = list(scrollX = TRUE, pageLength = 5)) %>% 
       formatRound(columns=2:17,digits=4)
+  })
+  
+  ## Toggle submit button state according to data ---------------
+  observe({
+    # if(nrow(forecast() == 0))
+    #   shinyjs::disable("plot_btn3")
+    # else 
+    if(nrow(forecast() > 0))
+      shinyjs::enable("plot_btn3")
+  })
+  
+  ## plot forecast -------------
+  output$ts_plot <- renderPlot({
+    # req(logistic_check()!="error")
+    g <- plot(p_model(), forecast())
+    g+theme_classic()
+  })
+  
+  ## plot prophet components --------------
+  output$prophet_comp_plot <- renderPlot({
+    # req(logistic_check()!="error")
+    prophet_plot_components(p_model(),forecast())
   })
   
 }
